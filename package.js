15fefd1446518139f5a59c6048ded4ff7b3a5c28
Ore Landau <Ore4444@gmail.com>
2013-07-17 16:32:25 +0300

    All plugins are now included, in their unminified state

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use('jquery', 'client');
-    api.add_files(['/lib/jquery.flot.min.js','/lib/jquery.flot.pie.min.js','/lib/jquery.flot.resize.min.js'], 'client');
+    api.add_files(['lib/jquery.colorhelpers.js', 'lib/jquery.flot.js', 'lib/jquery.flot.image.js', 'lib/jquery.flot.stack.js', 'lib/jquery.flot.canvas.js', 'lib/jquery.flot.symbol.js', 'lib/jquery.flot.categories.js', 'lib/jquery.flot.navigate.js', 'lib/jquery.flot.threshold.js', 'lib/jquery.flot.crosshair.js', 'lib/jquery.flot.pie.js', 'lib/jquery.flot.time.js', 'lib/jquery.flot.errorbars.js', 'lib/jquery.flot.resize.js', 'lib/jquery.flot.fillbetween.js', 'lib/jquery.flot.selection.js'], 'client');
 });
 
 Package.on_test(function (api) {
